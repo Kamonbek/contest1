98350836ff8c62ed5589091cc0d7b13ab96f1c09
Kamonbek <147366770+Kamonbek@users.noreply.github.com>
2025-02-13 19:31:57 +0500

    Update task3.py

--- src/task3.py
+++ src/task3.py
@@ -2,9 +2,9 @@ import time
 
 def decorator_1(func):
     def wrapper(*args, **kwargs):
-        start_time = time.time()  # Start time before function execution
-        func(*args, **kwargs)  # Call the actual function
-        end_time = time.time()  # End time after execution
-        execution_time = end_time - start_time  # Calculate elapsed time
+        start_time = time.time()  
+        func(*args, **kwargs)  
+        end_time = time.time()  
+        execution_time = end_time - start_time
         print(f"{func.__name__} call executed in {execution_time:.4f} sec")
     return wrapper
