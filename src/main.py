00e1b6f87a628e6d487058bc85f7027448fd5c35
Kamonbek <147366770+Kamonbek@users.noreply.github.com>
2025-02-13 19:33:09 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from task1 import kwargsAcceptFun
 from task2 import typeBasedTransformer
-from task3 import decorator_1
-kwargsAcceptFun(name="John", age=25, city="New York") 
-typeBasedTransformer(name="John", age=25, city="New York", wives=['kate','angela','julia'])
-decorator_1(kwargsAcceptFun)(name="John", age=25, city="New York")
 
+kwargsAcceptFun(name="John", age=25, city="New York", children=['kate','angela','julia'], is_student=True, gpa=3.5, height=5.8, weight=150, is_married=False, has_children=True) 
+typeBasedTransformer(name="John", age=25, city="New York", children=['kate','angela','julia'])
