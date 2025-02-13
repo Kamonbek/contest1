593c217a85f44b29a0ff1073c42a8b7fc5d7b543
Kamonbek <147366770+Kamonbek@users.noreply.github.com>
2025-02-13 19:32:11 +0500

    Update task2.py

--- src/task2.py
+++ src/task2.py
@@ -1,21 +1,19 @@
+from task3 import decorator_1
+@decorator_1
 def typeBasedTransformer(**kwargs):
     transformed = {}
     
     for key, value in kwargs.items():
-        if isinstance(value, (int, float)):  # Square numbers
+        if isinstance(value, (int, float)):  
             transformed[key] = value ** 2
-        elif isinstance(value, str):  # Reverse strings
-            transformed[key] = value[::-1]
-        elif isinstance(value, bool):  # Invert booleans
+        elif isinstance(value, bool):  
             transformed[key] = not value
-        elif isinstance(value, (list, tuple)):  # Reverse lists/tuples
+        elif isinstance(value, (list, tuple, str)): 
             transformed[key] = value[::-1]
-        elif isinstance(value, dict):  # Swap keys and values in dictionaries
-            if len(value) == len(set(value.values())):  # Ensure values are unique
+        elif isinstance(value, dict):  
+            if len(value) == len(set(value.values())):  
                 transformed[key] = {v: k for k, v in value.items()}
             else:
-                transformed[key] = value  # Leave unchanged if values are not unique
-        else:  # Leave unsupported types unchanged
-            transformed[key] = value
+                transformed[key] = value 
 
-    return transformed
+    print(transformed) 
