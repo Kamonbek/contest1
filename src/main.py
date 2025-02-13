0a649213a4706a3126571d98c869be95bb60d66f
Kamonbek <147366770+Kamonbek@users.noreply.github.com>
2025-02-13 19:13:19 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
+from task1 import kwargsAcceptFun
+from task2 import typeBasedTransformer
+from task3 import decorator_1
+kwargsAcceptFun(name="John", age=25, city="New York") 
+typeBasedTransformer(name="John", age=25, city="New York", wives=['kate','angela','julia'])
+decorator_1(kwargsAcceptFun)(name="John", age=25, city="New York")
 
